cb57e88fd864969d4e755d50c7247a8e37e60c53
sharapovshoxrux <shoxruxsharapov@gmail.com>
2025-05-07 13:59:17 -0400

    started a project and adding bunch of pages/compo

---
+++ src/components/Footer.js
@@ -0,0 +1,9 @@
+function Footer() {
+  return (
+    <footer>
+      <p>© {new Date().getFullYear()} no-mad.us</p>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ src/components/Layout.js
@@ -0,0 +1,17 @@
+import NavBar from "./NavBar";
+import Footer from "./Footer";
+import { Outlet } from "react-router-dom";
+
+function Layout() {
+  return (
+    <>
+      <NavBar />
+      <main>
+        <Outlet />
+      </main>
+      <Footer />
+    </>
+  );
+}
+
+export default Layout;
